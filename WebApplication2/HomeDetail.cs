1f3cf2bba7635e5344595325e06ac6e2f9c4ea94
mamir <mohammad.amir@charter.com>
2016-05-17 16:12:05 +0530

    Added new class : HomeDetail

---
+++ WebApplication2/HomeDetail.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2
+{
+    public class HomeDetail
+    {
+        public string AdressLine1
+        {
+            get; set;
+        }
+    }
+}
